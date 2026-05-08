d90121ae93da97f5a6d7997adc6668644e651739
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-05-08 09:36:14 +0200

    antwortoptionen.js: Output verbessert

--- src/main_antwortoptionen.js
+++ src/main_antwortoptionen.js
@@ -21,10 +21,13 @@ const prompt =
   `Erzeuge 4 Antwortoptionen für folgende Frage. \
    Die Antwortoptionen sollen kurz und stichpunktartig sein, \
    ohne Einleitungstexte oder Erklärungen; \
-   die richtige Antwort soll mit einem nachgestellten (richtig) markiert werden: \
+   die richtige Antwort soll mit einem nachgestellten (richtig) markiert werden \
+   und zuerst genannt werden: \
    ${eingabeStr}`;
 
 const ai = new GoogleGenAI({apiKey: API_KEY});
+
+console.log( "\nGeneriere Antwortoptionen...\n" );
 
 async function main() {
 const antwort = await ai.models.generateContent({
